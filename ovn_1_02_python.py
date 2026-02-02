e4516f902193acb0f0062be878e7b6fc220f0359
larstan5202 <lars.tangfelt@gmail.com>
2026-02-02 14:57:50 +0100

    Lars Test

---
+++ ovn_1_02_python.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    if i == 5:
+        print("")
+    else:
+        print(i)
+    i = i + 1
+
+# Blir en tomrad på rad 5
---
+++ tes.py
@@ -0,0 +1,7 @@
+limit = 15
+index = 5
+while index <= limit:
+    print(index)
+    index = index + 2
+
+# Skriver ut varannat tal
